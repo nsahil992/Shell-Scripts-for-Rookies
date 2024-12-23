0c184e7bc972ca40db2b36f81ed95870d4108cbc
Sahil Naik <nsahil992@gmail.com>
2024-12-23 19:39:23 +0530

    feat: Taking input from user

--- 03UserInput.sh
+++ 03UserInput.sh
@@ -5,8 +5,14 @@ read learning
 
 echo "Your are learning $learning"
 
+# Input with prompt
 read -p "Enter your favourite color: " color
 
 echo "Your favourite color is $color"
+
+# Silent prompt 
+read -s -p "Enter your password: " password
+echo
+echo "Password saved."
 
 
