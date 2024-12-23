664de48a96cf61309a660dea6626ea0b19dbeaf4
Sahil Naik <nsahil992@gmail.com>
2024-12-23 19:08:35 +0530

    feat: First Shell-Script

--- 01FirstShellScript.sh
+++ 01FirstShellScript.sh
@@ -3,6 +3,8 @@
 
 # We can write comment in the same format
 
+# Examples of multiline comments
+# Type 1:
 << comment
 This is a multi-line comment.
 We can write any number of lines in this section.
@@ -10,3 +12,17 @@ Primarily it is used to explain what our Shell Script does.
 comment
 
 echo "Shell-Script is amazing"
+
+
+#Type 2:
+
+: '
+This 
+is a 
+multiline
+comment
+'
+
+echo "Scripts execution continues after the comment"
+
+
